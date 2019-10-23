63ad33662c1dc1dd551c70b7a3fad46dc52ee1b0
flippenc <43418226+flippenc@users.noreply.github.com>
2019-10-22 20:07:03 -0400

    Adding comment to Course

--- CourseQuest/COURSEQUESTOFFICIAL/app/src/main/java/com/example/coursequest/Course.java
+++ CourseQuest/COURSEQUESTOFFICIAL/app/src/main/java/com/example/coursequest/Course.java
@@ -6,6 +6,11 @@ import android.os.Parcelable;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+This class encapsulates the information found by the web scraper
+This class was written by Christopher Flippen
+*/
+
 public class Course implements Parcelable
 {
     @Override
