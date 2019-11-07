79ca01a746c57047eb10ae1f76efd020affa049b
Jeetika <46970627+sainanij@users.noreply.github.com>
2019-11-07 15:40:39 -0500

    Working scraper

--- CourseQuest/COURSEQUESTOFFICIAL/app/src/main/java/com/example/coursequest/CourseraWebScraper.java
+++ CourseQuest/COURSEQUESTOFFICIAL/app/src/main/java/com/example/coursequest/CourseraWebScraper.java
@@ -1,23 +1,24 @@
 //Code by Jeetika Sainani
 package com.example.coursequest;
 
-import java.io.IOException;
-
+import android.widget.Button;
+import android.os.AsyncTask;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class CourseraWebScraper {
+public class CourseraWebScraper extends AsyncTask<Object, String, ArrayList<Course>>{
 
 		
 	private Document document;
+	SearchPageResults page;
 
-	public void search(String searchTerm){
-			
+
+	protected ArrayList<Course> doInBackground(Object... params) {
+		String searchTerm = (String) params[0];
+		page = (SearchPageResults) params[1];
 		List<String> titlesList = new ArrayList<String>();
 		List<String> univs = new ArrayList<String>();
 		List<String> types = new ArrayList<String>();
@@ -30,9 +31,8 @@ public class CourseraWebScraper {
 			
 			for(int i = 1; i < 4; i++) {
 
-				document = Jsoup.connect("https://www.coursera.org/search?query=" + searchTerm + "\"&indices%5Bprod_all_products%5D%5Bpage%5D=" + Integer.toString(i) + "&indices%5Bprod_all_products%5D%5Bconfigure%5D%5BclickAnalytics%5D=true&indices%5Bprod_all_products%5D%5Bconfigure%5D%5BhitsPerPage%5D=10&configure%5BclickAnalytics%5D=true\"").get();
-				
-				
+				document = Jsoup.connect("https://www.coursera.org/search?query=" + searchTerm + "&indices%5Bprod_all_products%5D%5Bpage%5D=" + Integer.toString(i) + "&indices%5Bprod_all_products%5D%5Bconfigure%5D%5BclickAnalytics%5D=true&indices%5Bprod_all_products%5D%5Bconfigure%5D%5BhitsPerPage%5D=10&configure%5BclickAnalytics%5D=true").get();
+
 				Elements titles = document.getElementsByClass("color-primary-text card-title headline-1-text");
 				titlesList.addAll(titles.eachText());
 				
@@ -50,26 +50,25 @@ public class CourseraWebScraper {
 				
 				Elements rating = document.getElementsByClass("ratings-text");
 				ratings.addAll(rating.eachText());
-				
-				
+
 			}
 			
-			getCourseLinks(allLinks, courseLinks);
-			makeCourseObjects(titlesList, ratings, courseLinks, "Coursera", searchTerm);
-					
 		}
 
 		catch(Exception e){
 			System.out.println("No search results");
 		}
+		getCourseLinks(allLinks, courseLinks);
+		return 	makeCourseObjects(titlesList, ratings, courseLinks, "Coursera", searchTerm);
+
 	}
 	
 	
 	public static void getCourseLinks(List<String> allLinks, List<String> courseLinks) {
 		for(int r = 0; r < allLinks.size(); r ++) {
-			String potential_Link = allLinks.get(r).toString();
-			if(potential_Link.contains("learn/") || potential_Link.contains("specialization") || potential_Link.contains("professional-certificates")) {
-				courseLinks.add("coursera.org" + potential_Link);
+			String potential_Link = allLinks.get(r);
+			if(potential_Link.contains("learn/") || potential_Link.contains("specializations/") || potential_Link.contains("professional-certificates/")) {
+				courseLinks.add("https://www.coursera.org" + potential_Link);
 			}
 		}
 	}
@@ -90,12 +89,17 @@ public class CourseraWebScraper {
 		return allCourses;
 		
 	}
-	
-	
-	public static void main(String[] args) {
-		CourseraWebScraper webScraper = new CourseraWebScraper();
-	    String searchTerm = "Biology";
-	    webScraper.search(searchTerm);
+
+
+	protected void onPostExecute(ArrayList<Course> list) {
+		try {
+			//creates course buttons with course info and displays them on SearchPageResults
+			ArrayList<Button> buttons = page.createButtons(list);
+			page.displayResults(list, buttons);
+		}
+		catch(Exception e){
+			System.out.println("Results creation unsuccessful");
+		}
 	}
 		
 }
