a0204e11ede731a413590445150a78104792d239
Sage Thompson <sagethompson85@gmail.com>
2019-11-10 19:39:36 -0500

    added "trending" courses to search page

--- CourseQuest/COURSEQUESTOFFICIAL/app/src/main/java/com/example/coursequest/SearchPage.java
+++ CourseQuest/COURSEQUESTOFFICIAL/app/src/main/java/com/example/coursequest/SearchPage.java
@@ -6,11 +6,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 
 public class SearchPage extends AppCompatActivity {
@@ -21,6 +22,8 @@ public class SearchPage extends AppCompatActivity {
     private Button filter;
     private static final String PREFS_NAME = "prefs";
     private static final String PREF_DARK_THEME = "dark_theme";
+    private LinearLayout popularSearches;
+    private String[] popularSearchTerms = new String[] {"Science", "Astronomy", "Calculus", "Interior Design"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +53,9 @@ public class SearchPage extends AppCompatActivity {
                 openSettingsPage();
             }
         });
+
+        popularSearches = (LinearLayout) findViewById(R.id.popularSearches);
+        populatePopularSearchesView(popularSearchTerms);
 
         filter = findViewById(R.id.filter);
         registerForContextMenu(filter);
@@ -82,14 +88,31 @@ public class SearchPage extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void openResults() {
+    public void openResults(String search) {
         Intent intent = new Intent(this, SearchPageResults.class);
+        intent.putExtra("message", search);
         startActivity(intent);
     }
 
     public void openSettingsPage() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    public void populatePopularSearchesView(String[] searchterms){
+        for(String term: searchterms) {
+            final Button b = new Button(this);
+            b.setTransformationMethod(null);
+            b.setText(term);
+            courseButtonFormatter.format(this, b);
+            popularSearches.addView(b);
+            b.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    openResults(b.getText().toString());
+                }
+            });
+        }
+
     }
 
     @Override
