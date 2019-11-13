67f2d3429e13483b673a3ace4511065f46496970
Jeetika <46970627+sainanij@users.noreply.github.com>
2019-11-13 18:17:47 -0500

    Working scraper

--- CourseQuest/COURSEQUESTOFFICIAL/app/src/main/java/com/example/coursequest/CourseraWebScraper.java
+++ CourseQuest/COURSEQUESTOFFICIAL/app/src/main/java/com/example/coursequest/CourseraWebScraper.java
@@ -7,6 +7,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,13 +24,10 @@ public class CourseraWebScraper extends AsyncTask<Object, String, ArrayList<Cour
 		String searchTerm = (String) params[0];
 		page = (SearchPageResults) params[1];
 		List<String> titlesList = new ArrayList<String>();
-		List<String> univs = new ArrayList<String>();
-		List<String> types = new ArrayList<String>();
-		List<String> difficulties = new ArrayList<String>();
 		List<String> allLinks = new ArrayList<String>();
 		List<String> courseLinks = new ArrayList<String>();
-		List<String> ratings = new ArrayList<String>();
 		List<String> descriptions = new ArrayList<>();
+		ArrayList<Course> allCourses = new ArrayList<>();
 		
 		try {
 			//first page of results
@@ -57,20 +56,8 @@ public class CourseraWebScraper extends AsyncTask<Object, String, ArrayList<Cour
 				Elements titles = document.getElementsByClass("color-primary-text card-title headline-1-text");
 				titlesList.addAll(titles.eachText());
 				
-				Elements university = document.getElementsByClass("partner-name");
-				univs.addAll(university.eachText());
-			
-				Elements courseType = document.getElementsByClass("productTypePill_vy0uva-o_O-gradientBg_98hw3a");
-				types.addAll(courseType.eachText());
-				
-				Elements difficulty = document.getElementsByClass("difficulty");
-				difficulties.addAll(difficulty.eachText());
-				
 				Elements links = document.getElementsByTag("a");
 				allLinks.addAll(links.eachAttr("href"));
-				
-				Elements rating = document.getElementsByClass("ratings-text");
-				ratings.addAll(rating.eachText());
 
 			}
 			
@@ -86,7 +73,16 @@ public class CourseraWebScraper extends AsyncTask<Object, String, ArrayList<Cour
 		}
 
 		getCourseLinks(allLinks, courseLinks);
-		return 	makeCourseObjects(titlesList, ratings, courseLinks, descriptions, "Coursera", searchTerm);
+		for(int i = 0; i < titlesList.size(); i++) {
+			Course courseObj = new Course();
+			courseObj.setCourseLink(allLinks.get(i));
+			courseObj.setCourseName(titlesList.get(i));
+			courseObj.setCourseSubject(searchTerm);
+			courseObj.setCourseWebsite("Coursera");
+			courseObj.setCourseDescription(descriptions.get(i));
+			allCourses.add(courseObj);
+		}
+		return allCourses;
 
 	}
 	
@@ -98,23 +94,6 @@ public class CourseraWebScraper extends AsyncTask<Object, String, ArrayList<Cour
 				courseLinks.add("https://www.coursera.org" + potential_Link);
 			}
 		}
-	}
-
-	
-	public ArrayList<Course> makeCourseObjects(List<String> titles, List<String> ratings, List<String> links, List<String> descriptions, String website, String subject) {
-		ArrayList<Course> allCourses = new ArrayList<Course>();
-		for(int i = 0; i < titles.size(); i++) {
-			Course courseObj = new Course();
-			courseObj.setCourseLink(links.get(i));
-			courseObj.setCourseName(titles.get(i));
-			courseObj.setCourseSubject(subject);
-			courseObj.setCourseWebsite(website);
-			courseObj.setRating(ratings.get(i));
-			courseObj.setCourseDescription(descriptions.get(i));
-			allCourses.add(courseObj);
-		}
-
-		return allCourses;
 	}
 
 
