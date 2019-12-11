da26eeef823b09e0e206b2e822f329c05fc7ce60
flippenc <43418226+flippenc@users.noreply.github.com>
2019-12-11 16:10:45 -0500

    More Rewritten Espresso Tests

---
+++ CourseQuest/COURSEQUESTOFFICIAL/app/src/androidTest/java/com/example/coursequest/HomeButtonTest.java
@@ -0,0 +1,71 @@
+/*
+Class name: HomeButtonTest.java
+Scenario: Given I go to the navigation bar, when I click on the home button, then it takes me to the homepage
+ */
+package com.example.coursequest;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@SuppressWarnings("deprecation")
+@RunWith(AndroidJUnit4.class)
+public class HomeButtonTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void homeButtonTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.homeButton),
+                        childAtPosition(
+                                allOf(withId(R.id.Navigation),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
---
+++ CourseQuest/COURSEQUESTOFFICIAL/app/src/androidTest/java/com/example/coursequest/SaveToHomeTest.java
@@ -0,0 +1,114 @@
+/*
+Class name: SaveToHomeTest.java
+Scenario: Given a user wants to view their saved courses after liking, when they visit the home page, their saved courses then display.
+ */
+package com.example.coursequest;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SaveToHomeTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void SaveToHome() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.searchButton2),
+                        childAtPosition(
+                                allOf(withId(R.id.Navigation),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction button = onView(
+                allOf(withText("Photography"),
+                        childAtPosition(
+                                allOf(withId(R.id.popularSearches),
+                                        childAtPosition(
+                                                withId(R.id.popularSearchesView),
+                                                0)),
+                                1)));
+        button.perform(scrollTo(), click());
+
+        ViewInteraction button2 = onView(
+                allOf(withText("Photography Capstone Project\n\nNo description. Click for more information about this course."),
+                        childAtPosition(
+                                allOf(withId(R.id.resultView),
+                                        childAtPosition(
+                                                withId(R.id.results),
+                                                0)),
+                                1)));
+        button2.perform(scrollTo(), longClick());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.likeButton),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.resultView),
+                                        2),
+                                0)));
+        appCompatButton2.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.homeButton),
+                        childAtPosition(
+                                allOf(withId(R.id.Navigation),
+                                        childAtPosition(
+                                                withId(R.id.rootView),
+                                                3)),
+                                0),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
